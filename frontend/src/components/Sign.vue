7cf12d7f16789b21ef3db415de27967afbfb33ee
chokim-01 <chokim159@naver.com>
2022-07-05 18:10:24 +0900

    [+] Add Sign in & up modal

---
+++ frontend/src/components/Sign.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <b-modal id="modal-Sign" centered hide-header="true" hide-footer="true">
+      <b-tabs content-class="mt-3" justified>
+        <!-- Sign In Section -->
+        <b-tab title="Sign In" active>
+          <form>
+            <div class="form-group">
+              <label>ID</label>
+              <input type="email" class="form-control form-control-lg" />
+            </div>
+            <div class="form-group">
+              <label>Password</label>
+              <input type="password" class="form-control form-control-lg" />
+            </div>
+            <button type="submit" class="btn btn-dark btn-lg btn-block">Sign In</button>
+            <p class="forgot-password text-right mt-2 mb-4">
+              <router-link to="/forgot-password">Forgot password ?</router-link>
+            </p>
+          </form>
+        </b-tab>
+        <!-- Sign Up Section -->
+        <b-tab title="Sign Up">
+          <form>
+            <div class="form-group">
+              <label>ID</label>
+              <input type="text" class="form-control form-control-lg"/>
+            </div>
+            <div class="form-group">
+              <label>NickName</label>
+              <input type="text" class="form-control form-control-lg"/>
+            </div>
+            <div class="form-group">
+              <label>Email address</label>
+              <input type="email" class="form-control form-control-lg" />
+            </div>
+            <div class="form-group">
+              <label>Password</label>
+              <input type="password" class="form-control form-control-lg" />
+            </div>
+            <button type="submit" class="btn btn-dark btn-lg btn-block">Sign Up</button>
+          </form>
+        </b-tab>
+      </b-tabs>
+    </b-modal>
+  </div>
+</template>
+<script>
+export default {
+  name:"SignIn"
+}
+</script>
