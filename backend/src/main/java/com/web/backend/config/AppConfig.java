7103fc8ee47f0654cdb91fb601e34c12bfcdb4ac
DESKTOP-0DRJLGM\jgkin <limjae0@gmail.com>
2022-07-05 19:02:46 +0900

    - Bean 설정 추가

--- backend/src/main/java/com/web/backend/config/AppConfig.java
+++ backend/src/main/java/com/web/backend/config/AppConfig.java
@@ -1,13 +1,22 @@
 package com.web.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
+    @Bean
     public MailSender mailSender(){
         return new JavaMailSenderImpl();
     }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
