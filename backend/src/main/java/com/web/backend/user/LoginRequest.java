fb14ec355c1447ad4e7c0a809900bfdb3c5a9060
DESKTOP-0DRJLGM\jgkin <limjae0@gmail.com>
2022-07-05 19:05:09 +0900

    - 로그인, 회원가입 요청 객체와 검증용 객체 추가

--- backend/src/main/java/com/web/backend/user/LoginRequest.java
+++ backend/src/main/java/com/web/backend/user/LoginRequest.java
@@ -2,26 +2,17 @@ package com.web.backend.user;
 
 import lombok.Getter;
 import lombok.ToString;
-import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
 @ToString
-public class UserCreateRequest {
-
+public class LoginRequest {
     @NotBlank
     @Size(min = 2, max = 20)
     private String userId;
     @NotBlank
     @Size(min = 2, max = 20)
-    private String email;
-    @NotBlank
-    @Size(min = 2, max = 20)
     private String plainPassword;
-    @NotBlank
-    @Size(min = 2, max = 20)
-    private String nickname;
-
 }
