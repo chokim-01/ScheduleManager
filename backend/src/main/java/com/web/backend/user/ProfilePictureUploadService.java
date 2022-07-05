a1426bbd6efd2cb9757642871a202940b9f063ff
DESKTOP-0DRJLGM\jgkin <limjae0@gmail.com>
2022-07-05 19:35:28 +0900

    - ProfilePictureUploadService 추가
    프로필 사진 업로드

---
+++ backend/src/main/java/com/web/backend/user/ProfilePictureUploadService.java
@@ -0,0 +1,51 @@
+package com.web.backend.user;
+
+
+
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ProfilePictureUploadService {
+    private final Environment environment;
+
+
+    public String fileUploadAndReturnFileName(MultipartFile file){
+
+
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+
+        String newFileName = UUID.randomUUID()+"."+extension;
+
+
+        try (
+
+                FileOutputStream fos = new FileOutputStream(environment.getProperty("upload.filepath") + newFileName);
+                InputStream is = file.getInputStream()) {
+
+
+            int readCount = 0;
+            byte[] buffer = new byte[1024];
+
+            while ((readCount = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, readCount);
+
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("file Save Error");
+        }
+
+
+
+        return newFileName;
+    }
+
+}
