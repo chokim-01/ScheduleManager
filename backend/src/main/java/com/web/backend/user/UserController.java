22b39faaa2ba72fcee5fce2e663b7eec740a0129
DESKTOP-0DRJLGM\jgkin <limjae0@gmail.com>
2022-07-05 19:09:21 +0900

    - UserController 추가
    createUser 입력받은 폼 검증 후 회원가입 호출
    email주소 리턴
    
    authorizationUser 이메일로 발송된 링크에서 인증키로 유저 검색
    유저가 있으면 인증처리.

---
+++ backend/src/main/java/com/web/backend/user/UserController.java
@@ -0,0 +1,39 @@
+package com.web.backend.user;
+
+import com.web.backend.logger.Trace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    private final CreateRequestValidator validator;
+
+    @InitBinder("userCreateRequest")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(validator);
+    }
+
+    @PostMapping("/user")
+    @Trace
+    public ResponseEntity<String> createUser(@Valid @RequestBody UserCreateRequest request){
+
+        userService.createUser(request);
+
+        String email = request.getEmail();
+
+        return ResponseEntity.ok(email);
+    }
+
+    @GetMapping("/user/check-email-auth/{key}")
+    public ResponseEntity<String> authorizationUser(String key){
+        userService.authorizationUser(key);
+        return ResponseEntity.ok().build();
+    }
+
+}
