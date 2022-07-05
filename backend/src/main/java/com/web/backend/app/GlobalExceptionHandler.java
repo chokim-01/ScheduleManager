3a0091f2bc54dc73030e21f0ea68ddfb213fe7b0
DESKTOP-0DRJLGM\jgkin <limjae0@gmail.com>
2022-07-05 18:47:22 +0900

    - GlobalExceptionHandler 추가

---
+++ backend/src/main/java/com/web/backend/app/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.web.backend.app;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e){
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
