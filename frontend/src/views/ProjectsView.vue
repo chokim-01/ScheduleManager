4d312da1c248f8675bc5e94614f35d5cb6220615
chokim-01 <chokim159@naver.com>
2022-07-14 15:46:35 +0900

    [+] Add ProjectsView sync with pjtList

---
+++ frontend/src/views/ProjectsView.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="projects-view">
+  <ProjectList :selected="selected" :countOfCard="countOfCard" />
+</div>
+</template>
+<script>
+import ProjectList from '../components/ProjectList.vue'
+
+export default {
+  name: "ProjectsView",
+  components:{
+    ProjectList
+  },
+  data() {
+    return {
+      selected: 'a',
+      countOfCard: 1
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+#projects-view {
+  display: table;
+  margin-top: 8%;
+  transform: translateX(50%);
+  width:50%;
+}
+
+</style>
