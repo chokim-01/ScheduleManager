1be8c66663ec25746496ab1d0032cef56036f34d
chokim-01 <chokim159@naver.com>
2022-08-01 16:21:56 +0900

    [+] item del modal

---
+++ frontend/src/components/modal/ItemAdd.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-dialog v-model="show" max-width="500px">
+    <v-card>
+      <v-card-title class="text-h5 grey lighten-2">
+        Add Todo
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" text>
+          Add
+        </v-btn>
+        <v-btn color="primary" text @click="show=false">
+          Cancel
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script>
+export default {
+  name: 'ItemAdd',
+  props: ['visible'],
+  computed: {
+    show: {
+      get() {
+        return this.visible
+      },
+      set(value) {
+        if (!value) {
+          this.$emit('close')
+        }
+      }
+    }
+  },
+  mounted() {
+    console.log(1423)
+  }
+}
+</script>
---
+++ frontend/src/components/modal/ItemDelete.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-dialog v-model="show" max-width="500px">
+    <v-card>
+      <v-card-title class="text-h5 grey lighten-2">
+        Delete {{type}}
+      </v-card-title>
+      <v-card-text>
+        really want delete this? {{content}}
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="error" text>
+          Delete
+        </v-btn>
+        <v-btn color="primary" text @click="show=false">
+          Cancel
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+
+  </v-dialog>
+</template>
+<script>
+export default {
+  name: 'ItemDelete',
+  props: ['visible','type','content'],
+  computed: {
+    show: {
+      get() {
+        return this.visible
+      },
+      set(value) {
+        if (!value) {
+          this.$emit('close')
+        }
+      }
+    }
+  }
+}
+</script>
