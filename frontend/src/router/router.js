7981dcf805470ba56e48dd11552f67c0a11354c2
chokim-01 <chokim159@naver.com>
2022-08-03 22:44:47 +0900

    [+] Add ProjectIssues, ProjectTodo page

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -6,19 +6,50 @@ import ProfileOverview from "../views/ProfileOverview.vue"
 import ProfileEdit from "../views/ProfileEdit.vue"
 import ProjectsView from "../views/ProjectsView.vue"
 import ProjectDetail from "../views/ProjectDetail.vue"
+import ProjectIssues from "../views/ProjectIssues.vue"
+import ProjectTodo from "../views/ProjectTodo.vue"
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-    mode: "history",
-    routes: [
-        {path: "/", component: HomePage},
-        {path: "/forgotPasswd", component: ForgotPasswd},
-        {path: '/profile/overview', component: ProfileOverview},
-        {path: '/profile/edit', component: ProfileEdit},
-        {path: '/projects/:flag', name: 'Projects', component: ProjectsView},
-        {path: '/project/:id/detail', name: 'ProjectDetail', component: ProjectDetail}
-    ]
+  mode: "history",
+  routes: [{
+      path: "/",
+      component: HomePage
+    },
+    {
+      path: "/forgotPasswd",
+      component: ForgotPasswd
+    },
+    {
+      path: '/profile/overview',
+      component: ProfileOverview
+    },
+    {
+      path: '/profile/edit',
+      component: ProfileEdit
+    },
+    {
+      path: '/projects/:flag',
+      name: 'Projects',
+      component: ProjectsView
+    },
+    {
+      path: '/project/:id/detail',
+      name: 'ProjectDetail',
+      component: ProjectDetail
+    },
+    {
+      path: '/project/:id/issues',
+      name: 'ProjectIssues',
+      component: ProjectIssues
+    },
+    {
+      path: '/project/:id/todo',
+      name: 'ProjectTodo',
+      component: ProjectTodo
+    }
+  ]
 })
 
 export default router;
